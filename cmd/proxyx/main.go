ba9dadce427894a03d1d9858747178b45c592857
otabek05 <otabektoshmukhammedov@gmail.com>
2026-01-21 18:13:54 +0900

    chore: clean some of them

--- cmd/proxyx/main.go
+++ cmd/proxyx/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"ProxyX/internal/cli"
 	"ProxyX/internal/platform"
-	//"ProxyX/internal/proxy"
-	//"ProxyX/pkg/config"
 	"fmt"
 	"log"
 	"os"
@@ -26,29 +24,11 @@ func main() {
 	}
 
 	cmd.Execute()
-
-	/*
-	
-	proxyConfig, err := config.LoadProxyXConfig()
-	if err != nil {
-		log.Fatalf("Failed to load proxy config: %v", err)
-	}
-
-	serverConfig, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	srv := proxy.NewServer(serverConfig, proxyConfig)
-	srv.Start()
-	*/
-	
 }
 
 func requireRoot() {
 	if os.Geteuid() != 0 {
 		fmt.Println("This command must be run with sudo (root privileges required)")
-		fmt.Println("Example: sudo proxyx services")
 		os.Exit(1)
 	}
 }
